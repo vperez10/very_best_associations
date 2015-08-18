00d322a643be3dfad4420df2adc0a109b123d0ee
vperez10 <vperez@chicagobooth.edu>
2015-08-18 15:27:03 -0400

    Working on validations

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,2 +1,4 @@
 class Neighborhood < ActiveRecord::Base
+  validates :name, :uniqueness => { :scope => :city }
+  validates :name, :city, presence: true
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,4 @@
 class Venue < ActiveRecord::Base
   validates :name, :uniqueness => { :scope => :address }
+  validates :name, :address, :neighborhood_id, presence: true
 end
