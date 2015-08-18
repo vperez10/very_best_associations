4425fbf3f7e43568ac96b425cd81c64b0fd882c7
vperez10 <vperez@chicagobooth.edu>
2015-08-18 15:50:26 -0400

    favorite validation

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,5 @@
 class Favorite < ActiveRecord::Base
+  validates :user_id, :dish_id, :venue_id, presence: true
+  validates :user_id, :uniqueness => { :scope => [:dish_id, :venue_id], message: ", please note that this item already exists. Please enter a new item." }
+
 end
