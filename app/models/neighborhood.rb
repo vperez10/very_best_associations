5f41f55f3945da437acc3dd225b4974fc5e6097c
vperez10 <vperez@chicagobooth.edu>
2015-08-19 16:26:26 -0500

    has_many/through

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,5 @@ class Neighborhood < ActiveRecord::Base
   validates :name, :uniqueness => { :scope => :city }
   validates :name, :city, presence: true
   has_many :venues
+  has_many :favorites, :through => :venues
 end
